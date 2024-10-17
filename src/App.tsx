e86e8939c22357e199b97a62fcb2035adebc0c82
Immanuel Diaz <immanuel.diaz@includedhealth.com>
2024-10-16 20:57:28 -0600

    update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
   useEffect(() => {
-    console.log(process.env);
+    console.log(JSON.stringify(process.env));
   },[])
   return (
     <>
