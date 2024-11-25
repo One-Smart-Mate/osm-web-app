dc63e8430a03ebe320eb4ecba19a38bf2062f261
Erick Garcia <erick.garcia.sal@gmail.com>
2024-11-24 18:31:08 -0600

    add tooltips to the create-update users forms

--- src/utils/localizations/Strings.ts
+++ src/utils/localizations/Strings.ts
@@ -317,4 +317,16 @@ export default class Strings {
   //warning notifications
   static restrictedAccessMessage =
     "Access Denied: Your role is limited to the app and does not grant permission to access the site. Please contact the administrator if you believe this is an error.";
+
+  // Register / Update Users Form Tooltips
+  static userNameTooltip = "Enter the full name of the user. Only letters are allowed.";
+  static emailTooltip = "Enter a valid email address for the user.";
+  static passwordTooltip = "Enter a secure password. It must be at least 8 characters long.";
+  static confirmPasswordTooltip = "Confirm the password to ensure it matches the original.";
+  static siteRfcTooltip = "Select the RFC of the site to which the user will be assigned.";
+  static uploadCardDataWithDataNetTooltip = "Enable this option if the user can upload card data using DataNet.";
+  static uploadCardEvidenceWithDataNetTooltip = "Enable this option if the user can upload card evidence using DataNet.";
+  static rolesTooltip = "Select one or more roles to assign to the user.";
+       
+
 }
