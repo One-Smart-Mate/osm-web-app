138d37162b8a89bb2998394293f954e97f20a89e
ErickGS28 <erick.garcia.sal@gmail.com>
2025-02-26 17:17:34 -0600

    Update apiSlice.ts
    I accidentally add the apiSlice file.

--- src/services/apiSlice.ts
+++ src/services/apiSlice.ts
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const apiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000", credentials: 'same-origin' }),
+  baseQuery: fetchBaseQuery({ baseUrl: import.meta.env.VITE_API_SERVICE, credentials: 'same-origin' }),
   tagTypes: ["User"],
   endpoints: (_) => ({}),
 });
