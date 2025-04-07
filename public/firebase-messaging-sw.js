a9f5f4484852a768a8ce4d31558f2c8a1debea18
Erick Garcia <20233tn149@utez.edu.mx>
2025-04-06 22:01:29 -0600

    env imports

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -3,14 +3,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.17.2/firebase-app-compat.js'
 importScripts('https://www.gstatic.com/firebasejs/9.17.2/firebase-messaging-compat.js');
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDylV3QjU-q6JlHV8AgrCm76kkwhqZXnCk",
-  authDomain: "osm-dev-c2f20.firebaseapp.com",
-  projectId: "osm-dev-c2f20",
-  storageBucket: "osm-dev-c2f20.firebasestorage.app",
-  messagingSenderId: "703295341497",
-  appId: "1:703295341497:web:029e8ccccc597d95dd2c68",
-  measurementId: "G-ML87MQMK6H"
-
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASURE_ID
 };
 
 firebase.initializeApp(firebaseConfig);
