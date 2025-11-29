821a6bb8556908df829f949215720694417a5e18
Erick Garcia <20233tn149@utez.edu.mx>
2025-11-28 21:58:39 -0600

    refresh btn 3

--- src/pages/components/RefreshButton.tsx
+++ src/pages/components/RefreshButton.tsx
@@ -3,18 +3,12 @@ import { Button, Tooltip } from "antd";
 import { ReloadOutlined } from "@ant-design/icons";
 import Strings from "../../utils/localizations/Strings";
 
-// Check if dev tools are enabled via environment variable
-// This allows the button to show in deployed "develop" environments, not just localhost
-const isDevToolsEnabled = import.meta.env.VITE_ENABLE_DEV_TOOLS === 'true';
-
 interface RefreshButtonProps {
   onRefresh: () => void;
   isLoading?: boolean;
   showText?: boolean;
   tooltip?: string;
   size?: "small" | "middle" | "large";
-  /** If true, only shows in development mode. Default: true */
-  devOnly?: boolean;
 }
 
 const RefreshButton: React.FC<RefreshButtonProps> = ({
@@ -23,13 +17,7 @@ const RefreshButton: React.FC<RefreshButtonProps> = ({
   showText = false,
   tooltip,
   size = "middle",
-  devOnly = true,
 }) => {
-  // If devOnly is true and dev tools are not enabled, don't render
-  if (devOnly && !isDevToolsEnabled) {
-    return null;
-  }
-
   const button = (
     <Button
       onClick={onRefresh}
